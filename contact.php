7466929f7b8d8bd4a7ee53159cbad191dab78bc2
laurentnunez <lnunez@hotmail.fr>
2022-06-16 10:59:11 +0200

    debug of send mail fonction

--- contact.php
+++ contact.php
@@ -4,13 +4,13 @@ $myadress = "laurent.nunez.dev@gmail.com";
 $name = $_POST['name'];
 $email =  $_POST['email'];
 $message = $_POST['message'];
-$headers = 'From: '. $email . "\r\n" .
-     'Reply-To:' . $email . "\r\n" .
-     'X-Mailer: PHP/' . phpversion();
 
-mail($myadress, $message, $headers)
+
+$themail = mail($myadress, $message, "From: ". $name . "depuis le portfolio" . "/r/n" . "Reply-to:" . $email)
 or die ("Il y a eu un petit soucis, le mail n'a pas été envoyé :(");
 
-echo "Le mail a bien été envoyé !"
-          
+if ($themail) {
+echo "<p>Le mail a bien été envoyé !</p>";
+}
+
 ?>
