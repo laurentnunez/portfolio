26931e87e6ba168eece9766b009724fc81035929
laurentnunez <lnunez@hotmail.fr>
2022-06-30 17:24:04 +0200

    modal project 1 done

--- js/app.js
+++ js/app.js
@@ -1,1 +1,31 @@
 console.log('coucou !')
+
+function showProject() {
+  const links = document.querySelectorAll('.projects__card-link');
+  const modals = document.querySelectorAll('.modal');
+  const btns = document.querySelectorAll('.modal__close');
+
+  const resetModal = () => {
+    modals.forEach(modal=> {
+      modal.classList.remove('show')
+    });
+  }
+
+  links.forEach(element=>{
+    element.addEventListener('click',(event)=>{
+      event.preventDefault();
+      document.querySelector(`[id=${element.dataset.id}]`).classList.add('show');
+    });
+  });
+
+  btns.forEach(btn =>{
+    btn.addEventListener('click',(event)=>{
+      resetModal();
+      
+    });
+  });
+  
+
+}
+
+showProject();
